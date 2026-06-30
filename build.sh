7fb24adddec43d2e3ded9f509148b4a792ccc6c7
MATIAS5645 <matiasgregorio@outlook.cl>
2026-06-30 00:46:54 -0400

    Version 2.7

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# 💡 LÍNEA NUEVA: Crea el superusuario automáticamente si no existe
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'TuContraseñaSegura123')"
