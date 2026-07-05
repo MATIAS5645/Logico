db0f55fe1e2e4b6e5e0fb48887d11138271171c0
MATIAS5645 <matiasgregorio@outlook.cl>
2026-07-04 23:03:22 -0400

    version 3.9

--- logico_core/urls.py
+++ logico_core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from operaciones import views
-from . import views
+from operaciones import views as operaciones_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
